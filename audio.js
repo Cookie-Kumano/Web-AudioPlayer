70b7b59dadac3b3aa7954deeb3b914acb5a94af0
Cookie-Kumano <cookie@kumano.moe>
2021-08-28 16:17:48 +0900

    内部処理の変更

--- audio.js
+++ audio.js
@@ -13,33 +13,30 @@ function setMusicBox() {
     musicbox = document.getElementsByClassName("musicbox");
 
     for (let i = 0; i < musicbox.length; i++) {
-        musicbox[i].getElementsByClassName("play")[0].addEventListener("click", {name: i, handleEvent: playMusic}, false);
-        musicbox[i].getElementsByClassName("seekbar")[0].value = 0; 
-        musicbox[i].getElementsByClassName("seekbar")[0].style.backgroundSize = "0%";
-        musicbox[i].getElementsByClassName("seekbar")[0].disabled = true;
-        musicbox[i].getElementsByClassName("volume")[0].style.backgroundSize = "80%"
-        musicbox[i].getElementsByClassName("volume")[0].disabled = true;
+        playMusic(i);
     }
-
 }
 
-function playMusic() {
-    let audio = musicbox[this.name].children[0];
-    let seekbar = musicbox[this.name].getElementsByClassName("seekbar")[0];
-    let slider_volume = musicbox[this.name].getElementsByClassName("volume")[0];
+function playMusic(id) {
+    let playButton = musicbox[id].getElementsByClassName("play")[0];
+    let audio = musicbox[id].children[0];
+    let seekbar = musicbox[id].getElementsByClassName("seekbar")[0];
+    let slider_volume = musicbox[id].getElementsByClassName("volume")[0];
 
-    // 音量の設定
+    // 音量の設定と初期表示作成
+    seekbar.style.backgroundSize = "0%";
+    slider_volume.style.backgroundSize = "80%"
     audio.volume = slider_volume.value;
-
-    //スライドバー類の操作許可
-    seekbar.disabled  = false;
-    slider_volume.disabled = false;
-
-    if (audio.paused) {
-        audio.play();
-    }   else {
-        audio.pause();
-    }
+    
+    // 再生ボタン動作
+    playButton.addEventListener("click", (e) => {
+        if (audio.paused) {
+            audio.play();
+            
+        }   else {
+            audio.pause();
+        }
+    });
 
     // 現在の再生位置を表示
     audio.addEventListener("timeupdate", (e) => {
@@ -47,8 +44,8 @@ function playMusic() {
         const duration = Math.round(audio.duration);
         seekbar.max = Math.round(duration);
         if (!isNaN(duration)) {
-            $(musicbox[this.name]).find("#current").text(playTime(current));
-            $(musicbox[this.name]).find("#duration").text(playTime(duration));
+            $(musicbox[id]).find("#current").text(playTime(current));
+            $(musicbox[id]).find("#duration").text(playTime(duration));
             seekbar.value = Math.floor((audio.currentTime / audio.duration) * audio.duration);
             seekbar.style.backgroundSize = Math.floor((seekbar.value / seekbar.max) * 100) + "%"; 
         }
@@ -57,7 +54,6 @@ function playMusic() {
     // プログレスバーを引っ叩くと再生位置を変えられる
     seekbar.addEventListener("input", (e) => {
         audio.currentTime = seekbar.value;
-        console.log("操作" + seekbar.value);
     });
 
     // 音量スライダーの実装
@@ -70,7 +66,7 @@ function playMusic() {
     audio.addEventListener("ended", (e) => {
         seekbar.value = 0;
         seekbar.style.backgroundSize = "0%";
-        $(musicbox[this.name]).find("#current").text("00:00");
+        $(musicbox[id]).find("#current").text("00:00");
     });
 }
 
@@ -86,6 +82,5 @@ function playTime (t) {
     else {
         resTime = "00:" + Math.floor(t % 60).toString().padStart(2, "0");
     }
-
     return resTime;
   }
